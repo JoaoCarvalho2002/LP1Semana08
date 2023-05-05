0dcb4b1020c5393cfd60eb6922fdc1d302482cb7
JoaoCarvalho2002 <jonitkick@gmail.com>
2023-05-05 11:10:32 +0100

    exercicio 5

---
+++ GameUnits/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace GameUnits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        public class Unit
+        {
+            private int movement ;
+            public int Health{get;set;}
+        
+            public Unit (int movement,int health) {
+            this.movement = movement ;
+            Health = health ;
+            }
+            public void Move(){
+
+                Console.WriteLine("okkk");
+            }
+        }
+
+        
+    }
+}
